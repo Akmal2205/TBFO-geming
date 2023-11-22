e1049cc651a688a5529b045d029830b1d5836146
Akmal2205 <syarafiakml2004@gmail.com>
2023-11-22 23:10:01 +0700

    Update main.py

--- main_program/main.py
+++ main_program/main.py
@@ -1,23 +1,3 @@
-from bs4 import BeautifulSoup
-import re
 
-tags=[]
-all_nonspace=[]
-
-with open('c:\\Users\\ASUS\\Documents\\TBFO\\TBFO-geming\\test_files\\index.html', 'r', encoding='utf-8') as file:
-    # Read the entire content of the file into a variable
-    soup = BeautifulSoup(file, "html.parser")
-
-    all_tags = soup.find_all()
-
-for tag in all_tags:
-    tags+=[tag.name]
-
-with open('c:\\Users\\ASUS\\Documents\\TBFO\\TBFO-geming\\test_files\\index.html', 'r', encoding='utf-8') as file:
-    # Read the entire content of the file into a variable
-    for line in file:
-        all_nonspace+=re.split(r'[<,>]',line.strip())
-
-print(all_nonspace)
 
 
